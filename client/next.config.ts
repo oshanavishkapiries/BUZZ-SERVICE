5a9c924832ce642966f2af250be3477a443c5908
oshanavishkapiries <oshanavishka06@gmail.com>
2026-06-19 18:09:07 +0000

    feat : add - mcp -server

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  allowedDevOrigins: ['3000-cs-9c0df78f-cdba-48ab-b683-3fe75f6082c4.cs-asia-southeast1-palm.cloudshell.dev']
 };
 
 export default nextConfig;
